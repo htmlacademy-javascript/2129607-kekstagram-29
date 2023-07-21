23339b04ebf1a680a076f4421c2bebc358bd6e7c
Bogolepova Olga <olya1218@yandex.ru>
2023-07-21 15:22:20 +0300

    module7

---
+++ js/minis.js
@@ -0,0 +1,18 @@
+import {createDescriptions} from './create-descriptions.js';
+
+const minisList = document.querySelector('.pictures');
+const minisTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const minis = createDescriptions();
+const minisListFragment = document.createDocumentFragment();
+
+
+minis.forEach(({url, description, comments, likes}) => {
+  const minisElement = minisTemplate.cloneNode(true);
+  minisElement.querySelector('.picture__img').src = url;
+  minisElement.querySelector('.picture__img').alt = description;
+  minisElement.querySelector('.picture__comments').textContent = comments.length;
+  minisElement.querySelector('.picture__likes').textContent = likes;
+  minisListFragment.appendChild(minisElement);
+});
+
+minisList.appendChild(minisListFragment);
